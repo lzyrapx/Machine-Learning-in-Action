4a5af2a25ba540981e598345ab50a0ee5a446aeb
LzyRapx <1303377506@qq.com>
2018-02-28 21:54:43 +0800

    Add files via upload

---
+++ ch5/matplotlib/sigmoid.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+import sys
+from pylab import *
+
+t = arange(-60.0, 60.3, 0.1)
+s = 1/(1 + exp(-t))
+ax = subplot(211)
+ax.plot(t,s)
+ax.axis([-5,5,0,1])
+plt.xlabel('x')
+plt.ylabel('Sigmoid(x)')
+ax = subplot(212)
+ax.plot(t,s)
+ax.axis([-60,60,0,1])
+plt.xlabel('x')
+plt.ylabel('Sigmoid(x)')
+show()
